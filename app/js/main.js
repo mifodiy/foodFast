22d3ba264540dcf7aa3ad92cbe7202e793e6658f
mifodiy <mifodiy911@gmail.com>
2023-02-04 15:34:04 +0200

    Add review section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function () {
 
+  const reviewSwiper = new Swiper('.reviews__slider', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+  });
+
   AOS.init();
 
   const mixer = mixitup('.categories__list');
