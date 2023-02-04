5710c0ef9a3c3541557b4ecac8d3609c576f6959
mifodiy <mifodiy911@gmail.com>
2023-02-04 22:03:20 +0200

    Add catalog section, script range slider

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,64 @@ $(function () {
 
   });
 
+  let $range = $(".price__range-slider"),
+  $inputFrom = $(".price__input--from"),
+  $inputTo = $(".price__input--to"),
+  instance,
+  min = 0,
+  max = 1200,
+  from = 0,
+  to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    from: 100,
+    to: 1000,
+    onStart: updateInputs,
+    onChange: updateInputs
+  });
+  instance = $range.data("ionRangeSlider");
+
+  function updateInputs(data) {
+    from = data.from;
+    to = data.to;
+
+    $inputFrom.prop("value", from);
+    $inputTo.prop("value", to);
+  }
+
+  $inputFrom.on("input", function () {
+    var val = $(this).prop("value");
+
+    if (val < min) {
+      val = min;
+    } else if (val > to) {
+      val = to;
+    }
+
+    instance.update({
+      from: val
+    });
+  });
+
+  $inputTo.on("input", function () {
+    var val = $(this).prop("value");
+
+    if (val < from) {
+      val = from;
+    } else if (val > max) {
+      val = max;
+    }
+
+    instance.update({
+      to: val
+    });
+  });
+
+
   AOS.init();
 
   const mixer = mixitup('.categories__list');
